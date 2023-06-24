9be5032ff1ac1a05ebd39ab901f299a2ebb1f33c
Huge Letters <ckuka71@gmail.com>
2023-06-24 20:44:59 +0200

    some more mids

--- playground/medium/03188-medium-tuple-to-nested-object.ts
+++ playground/medium/03188-medium-tuple-to-nested-object.ts
@@ -18,16 +18,21 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToNestedObject<T, U> = any
+type TupleToNestedObject<T extends string[], U> = T extends [
+  infer First extends string,
+  ...infer Rest extends string[]
+]
+  ? { [K in First]: TupleToNestedObject<Rest, U> }
+  : U
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
 
 type cases = [
-  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
-  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
-  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
-  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
+  Expect<Equal<TupleToNestedObject<["a"], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<["a", "b"], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<["a", "b", "c"], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>
 ]
 
 /* _____________ Further Steps _____________ */
